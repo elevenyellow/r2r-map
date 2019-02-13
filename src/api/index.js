9db613e3d2f835138ac7680917341e876f279720
Josema <enzo@josema.es>
2019-02-13 17:47:58 -0500

    Building offset sprite as property not being calculated from radius/area

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,6 @@ export const OWNER = {
 
 export function createTileFactory({ ui, scene, camera }) {
     return ({ col, row, spriteConf }) => {
-        const offset = spriteConf.radius / 2
         const coordinate = `${col}.${row}`
         const sprite = addBuildingSprite({
             scene,
@@ -32,9 +31,9 @@ export function createTileFactory({ ui, scene, camera }) {
             coordinate,
             updatePositionDiv: ({ canvasWidth, canvasHeight }) => {
                 const proj = position3dToScreen2d({
-                    x: sprite.position.x + offset,
+                    x: sprite.position.x + spriteConf.offsetX,
                     y: sprite.position.y,
-                    z: sprite.position.z + offset,
+                    z: sprite.position.z + spriteConf.offsetZ,
                     camera,
                     canvasWidth,
                     canvasHeight
